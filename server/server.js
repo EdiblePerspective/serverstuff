8c9e38005144ee45506500f76136bb83d536c761
EdiblePerspective <ryancox430@gmail.com>
2023-12-19 10:28:00 +0000

    hjhjhjhjjh

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+// your routes will go here...
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000...");
+});
+app.get("/messages", (req, res) => {
+    res.json({ message: "Hello, World!" });
+  });
+  app.post("/messages", (req, res) => {
+    res.json({ message: "Hello, World!" });
+  });
+  app.use(express.json());
